866b0d2cf7c739751d44a5e20675dee62018a024
JoelAmores95 <joel.amores95@gmail.com>
2022-10-20 17:08:44 +0200

    Add Counter

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+// Dice que es importante que es ASPNET CORE
 
 namespace PrimerProjecte.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+
+// Hereda de una clase, solo una. Tantas interficies como quiera
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -11,8 +14,10 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+// Patron de inyeccion de dependencias, crea el minimo de instancias posibles, por eso se crea DENTRO del constructor
     private readonly ILogger<WeatherForecastController> _logger;
 
+    // Esta depende de la de arriba
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
@@ -29,4 +34,11 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+    [HttpPost]
+    public void Post([FromBody] WeatherForecast forecast)
+    {
+        _logger.LogInformation(forecast.ToString());
+    }
+   
 }
---
+++ ICounterService.cs
@@ -0,0 +1,7 @@
+namespace PrimerProjecte
+{
+    public interface ICounterService
+    {
+        int getAddIncrement();
+    }
+}
